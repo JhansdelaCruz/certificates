0c852345f0311f2c8563064dd0be3c87839360ad
Jhans de la Cruz <jhansdlcd@gmail.com>
2023-08-04 22:22:40 -0500

    style: trackCert

--- src/components/TracksCert/TracksCert.jsx
+++ src/components/TracksCert/TracksCert.jsx
@@ -7,25 +7,35 @@ const urlImage = require.context('../../media/imgCertificates/xs/', true);
 
 const TracksCert = () => {
   return (
-    <Container maxWidth="lg">
+    <Container
+      sx={{ paddingLeft: { xs: '4rem', sm: '2rem' }, paddingRight: { xs: '3rem', sm: '2rem' } }}
+    >
       <ImageList
         variant="masonry"
-        gap={'2.2rem'}
+        gap={'4rem'}
         sx={{
           columnCount: {
             xs: '1 !important',
             sm: '2 !important',
             md: '3 !important',
           },
+          borderRadius: '13px',
         }}
       >
         {datos.map((item) => (
-          <ImageListItem key={item.img}>
+          <ImageListItem
+            key={item.img}
+            sx={{
+              boxShadow: '10px 10px 20px rgba(25,27,38,0.8)',
+              borderRadius: '13px',
+            }}
+          >
             <img
               src={urlImage(`./${item.img}`)}
               srcSet={urlImage(`./${item.img}`)}
               alt={item.title}
               loading="lazy"
+              style={{ borderTopLeftRadius: '13px', borderTopRightRadius: '13px' }}
             />
 
             <ImageListItemBar
